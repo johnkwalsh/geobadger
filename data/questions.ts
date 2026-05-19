a2de49d878414c98d1df197f8373e693c146d2b1
johnkwalsh <johnkirkwalsh@gmail.com>
2026-05-19 12:28:24 -0500

    Update questions.ts

--- data/questions.ts
+++ data/questions.ts
@@ -1,32 +1,39 @@
-[
+export type QuizQuestion = {
+  prompt: string;
+  answerLabel: string;
+  lat: number;
+  lng: number;
+};
+
+export const questions: QuizQuestion[] = [
   {
     prompt: "Vitamin C was first isolated here.",
     answerLabel: "UW Biochemistry Building",
     lat: 43.0767,
-    lng: -89.4125
+    lng: -89.4125,
   },
   {
     prompt: "Barry Alvarez coached his first game as Wisconsin head coach here.",
     answerLabel: "Camp Randall Stadium",
     lat: 43.0699,
-    lng: -89.4128
+    lng: -89.4128,
   },
   {
     prompt: "The Onion was founded by UW–Madison students here.",
     answerLabel: "Memorial Union",
     lat: 43.0769,
-    lng: -89.4003
+    lng: -89.4003,
   },
   {
     prompt: "Students jump into this lake after major Badger victories.",
     answerLabel: "Lake Mendota",
     lat: 43.0997,
-    lng: -89.4218
+    lng: -89.4218,
   },
   {
     prompt: "The famous UW tradition of rubbing Lincoln’s shoe for good luck happens here.",
     answerLabel: "Abraham Lincoln Statue at Bascom Hill",
     lat: 43.0755,
-    lng: -89.4041
-  }
-]
+    lng: -89.4041,
+  },
+];
